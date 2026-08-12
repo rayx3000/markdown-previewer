6dd82e9eea41379f16f02379ab32d0498ab582c3
rayx3000 <candole2020@gmail.com>
2026-08-12 11:12:12 +0800

    add new files

---
+++ src/components/Editor/Editor.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Editor.scss'
+
+const Editor = () => {
+  return (
+    <div>Editor</div>
+  )
+}
+
+export default Editor
---
+++ src/components/Previewer/Previewer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Previewer.scss'
+
+const Previewer = () => {
+  return (
+    <div>Previewer</div>
+  )
+}
+
+export default Previewer

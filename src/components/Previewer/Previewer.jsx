1b692866497c2af0cf3b0f6020d76403a5292e64
rayx3000 <candole2020@gmail.com>
2026-09-21 11:18:27 +0800

    add previewer and make editor better GUI

--- src/components/Previewer/Previewer.jsx
+++ src/components/Previewer/Previewer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Previewer.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFile, faMaximize } from '@fortawesome/free-solid-svg-icons'
 
 const Previewer = () => {
   return (
-    <div></div>
+    <div className='previewer'>
+      <div className="toolbar">
+          <div className="toolbar-left">
+            <FontAwesomeIcon icon={faFile} />
+            <span>Previewer</span>
+          </div>
+          <div className="toolbar-right">
+            <FontAwesomeIcon icon={faMaximize} />
+          </div>
+      </div>
+      <div className="previewer-body"></div>
+    </div>
   )
 }
 
